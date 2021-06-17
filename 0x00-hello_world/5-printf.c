d37d425941da259c919af3c5903afd7d4d1ea11a
quaja-abu <acabuchane@gmail.com>
2021-06-17 12:57:34 +0300

    adding a c sourc code that will display text with printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * display text using prinf function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
