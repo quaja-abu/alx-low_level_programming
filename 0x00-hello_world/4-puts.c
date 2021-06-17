c41b759fbf82ce08f911daa42baf52f767acf909
quaja-abu <acabuchane@gmail.com>
2021-06-17 12:51:11 +0300

    modifing 4-puts.c source code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
+/**
+ * main - Entry
+ * display text with puts function
+ * Return: Always 0 (Success)
+ */
 #include <stdio.h>
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
