fe30a5cd0b035b488468519bc0545597959322de
quaja-abu <acabuchane@gmail.com>
2021-06-18 15:26:22 +0300

    program that prints small alphabet in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ * program to print alphabet in reverse order
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
