7ea653ccc4f0b105f5feda186a2a3d16c33ad8cf
quaja-abu <acabuchane@gmail.com>
2021-06-18 17:54:17 +0300

    fix output

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -26,8 +26,12 @@ int main(void)
 				putchar('0' + i);
 				putchar('0' + j);
 				putchar('0' + k);
-				putchar(',');
-				putchar(' ');
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
 			}
 		}
 	}
