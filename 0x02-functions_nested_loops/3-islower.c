be4f224ff5c829a10d96948c59f48e4be0269f98
quaja-abu <acabuchane@gmail.com>
2021-06-22 14:52:12 +0300

    fix betty documentation style

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - function to check for lowercase character
- * c - an integer parameter for _islower function
+ * @c:  an integer parameter for _islower function
  * Return: 1 if character lowecase 0 otherwise
  */
 int _islower(int c)
