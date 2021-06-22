1a269c93a9fe15bdc9aa0ac67528ebf4cf5590da
quaja-abu <acabuchane@gmail.com>
2021-06-22 17:52:51 +0300

    a program that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - to add to integer numbers
+ * @a: an integer argument to hold first value
+ * @b: an integer argument to hold second value
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
