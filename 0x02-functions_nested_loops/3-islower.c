c2b67781573f43a1e415dcf00db3a188ab59a1c9
quaja-abu <acabuchane@gmail.com>
2021-06-22 14:07:55 +0300

    a program that checks for lowercase chracter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _islower - function to check for lowercase character
+ * c - an integer parameter for _islower function
+ * Return: 1 if character lowecase 0 otherwise
+ */
+int _islower(int c)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (c == ch)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
