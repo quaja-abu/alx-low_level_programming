c697c821cca915c71c443d1070b7b1da17b476ce
quaja-abu <acabuchane@gmail.com>
2021-06-23 13:47:18 +0300

    fix output

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,17 +9,17 @@ int main(void)
 {
 	long int i = 1;
 	long int j = 2;
-	long int sum = 0;
+	unsigned long int sum = 0;
 	int count = 2;
 
 	sum = i + j;
-	printf("%ld%c%c%ld%c%c%ld%c%c", i, ',', ' ', j, ',', ' ', sum, ',', ' ');
+	printf("%ld%c%c%ld%c%c%lu%c%c", i, ',', ' ', j, ',', ' ', sum, ',', ' ');
 	while (count <= 95)
 	{
 		i = j;
 		j = sum;
 		sum = i + j;
-		printf("%ld", sum);
+		printf("%lu", sum);
 		if (count < 95)
 		{
 			printf("%c%c", ',', ' ');
