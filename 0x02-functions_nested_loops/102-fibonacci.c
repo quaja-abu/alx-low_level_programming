115b9d4dc2113d2c18fd6ff48ebdc9c0bf7ab331
quaja-abu <acabuchane@gmail.com>
2021-06-23 11:46:44 +0300

    program that prints the first 50 fibonacci series

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i = 1;
+	int j = 2;
+	int sum = 0;
+
+	sum = i + j;
+	printf("%d%c%c%d%c%c%d%c%c", i, ',', ' ', j, ',', ' ', sum, ',', ' ');
+	while (sum < 50)
+	{
+		i = j;
+		j = sum;
+		sum = i + j;
+		printf("%d", sum);
+		if (i != 50)
+		{
+			printf("%c%c", ',', ' ');
+		}
+	}
+	printf("\n");
+}
