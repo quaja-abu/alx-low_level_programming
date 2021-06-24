58fba965478f61c3f9746f7dc6f77d5cf4885244
quaja-abu <acabuchane@gmail.com>
2021-06-24 12:30:52 +0300

    fix output

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -7,7 +7,7 @@
  * positive_or_negative - function prototype
  */
 
-void positive_or_negative(int i);
+int positive_or_negative(int i);
 int largest_number(int, int, int);
 int convert_day(int, int);
 void print_remaining_days(int, int, int);
