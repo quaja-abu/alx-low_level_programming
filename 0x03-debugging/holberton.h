49b59ebe947eeef370cff622c421f7120e373a41
quaja-abu <acabuchane@gmail.com>
2021-06-24 14:41:15 +0300

    fix beety doc

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -6,10 +6,12 @@
 /*
  * positive_or_negative - function prototype
  * largets_number - find the largets of 3 numbers
+ * convert_day - convert the day
+ * print_remaining_days - calculate remaing days
  */
 
-int positive_or_negative(int i);
-int largest_number(int a, int b, int c);
+int positive_or_negative(int);
+int largest_number(int, int, int);
 int convert_day(int, int);
 void print_remaining_days(int, int, int);
 
