b467da6a9ce69f588322d75e3389382ec20c9155
quaja-abu <acabuchane@gmail.com>
2021-06-28 15:51:58 +0300

    program to print sting in reverse following by new line

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_rev - function to print sting in reverse
+ * @s: character pointer
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int count = 0;
+	int j = 0;
+
+	while (*(s + i) != '\0')
+	{
+		count++;
+		i++;
+	}
+	while (j <= count)
+	{
+		_putchar(*(s + count));
+		count--;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -7,11 +7,13 @@
  * swap_int - function to swap two integer values using pointer
  * _strlen - function that returns length of a string
  * _puts - a function that prints a string, followed by a new line
+ * print_rev - function that prints string in reverse 
  */
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+void print_rev(char *s);
 
 #endif /* HOLBERTON_H */
