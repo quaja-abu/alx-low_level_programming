a9c8906f6bfccccd3dc8a0af41282e4c2ab656f3
quaja-abu <acabuchane@gmail.com>
2021-06-28 13:44:44 +0300

    program that resets integer value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - function that take a pointer to an int as parameter
+ * @n: interger pointer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* 
+ * _putchar - function to define _putchar that prints characters
+ * reset_to_98 - function to takes a pointer to an int as parameter
+ */
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif /* HOLBERTON_H */
