545c5ec91e9551b23600e5b793f25adff4d5e0e4
quaja-abu <acabuchane@gmail.com>
2021-06-30 13:54:55 +0300

    program to concatinate two strings with a specific size

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -4,9 +4,11 @@
 /*
  * _putchar - function for printing character
  * _strcat - function to concatinate two strings
+ * _strncat - function to concatinate two string at most n size
  */
 
 int _putchar(char *s);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 #endif /* HOLBERTON_H */
