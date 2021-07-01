42172dfa247a3c0061ce1759d04ed587f0f5772f
quaja-abu <acabuchane@gmail.com>
2021-07-01 22:46:19 +0300

    program to compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,42 +1,17 @@
 #include "holberton.h"
 
-/**
- * _strcmp - function to compare two strings
- * @s1: character pointer of first string
- * @s2: character pointer of second string
- * Return: less than zero, zero or greater than zero integer
- */
-
 int _strcmp(char *s1, char *s2)
 {
-	int sw = 1;
 	int i = 0;
-	int j = 0;
-	int k = 0;
-	int lens = 0;
-	int lend = 0;
 	int val = 0;
 
-	while (s1[j] != '\0')
-	{
-		lens++;
-		j++;
-	}
-	while (s2[k] != '\0')
-	{
-		lend++;
-		k++;
-	}
-	while (sw)
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
 		if (s1[i] == s2[i])
 		{
-			if (lens == lend)
-			{
-				break;
-			}
-			continue;
+			val = s1[i] - s2[i];
 			i++;
+			continue;
 		}
 		else
 		{
