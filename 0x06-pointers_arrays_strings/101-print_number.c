e14dca1aa813a54345ed266df17aabcaa125ac07
quaja-abu <acabuchane@gmail.com>
2021-07-01 20:34:19 +0300

    program to print numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_number - function to print number using _putchar
+ * @n: interger value
+ */
+
+void print_number(int n)
+{
+	unsigned int num = n;
+	int sign = -1;
+	if (n < 0)
+	{
+		_putchar('-');
+		num = sign * num;
+	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
+}
