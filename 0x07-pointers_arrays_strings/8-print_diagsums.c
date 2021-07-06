41fac1321b79ba21d2e14fa1785d989f6a19ab3b
quaja-abu <acabuchane@gmail.com>
2021-07-06 17:35:32 +0300

    fix betty style

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - function that prints the sum of two diagonals of asquare 
+ * print_diagsums - function that prints the sum of two diagonals of asquare
  * @a: integer pointer
  * @size: integer value for the size of the array
  */
@@ -22,18 +22,16 @@ void print_diagsums(int *a, int size)
 	}
 	printf("%d, ", sum);
 	sum = 0;
-	 for (i = 0; i < size; i++)
-        {
-                for (j = 0; j < size; j++)
-                {
-                        if (i + j == size - 1)
-                        {
-                                sum =  sum + *((a + i) + j);
-                        }
-                }
-        }
-	 printf("%d", sum);
-	 printf("\n");
-
-
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i + j == size - 1)
+			{
+				sum =  sum + *((a + i) + j);
+			}
+		}
+	}
+	printf("%d", sum);
+	printf("\n");
 }
