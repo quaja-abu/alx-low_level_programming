a686ba698f3ffe7128c4fad18dc38d95a83d5401
Bantegize Addis Alemayehu <acabuchane@gmail.com>
2021-07-07 09:35:15 +0000

    fix code with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,10 +9,12 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	while (s[i])
+	if (s[i] != '\0')
 	{
 		_putchar(s[i]);
-		i++;
+		++i;
+		_puts_recursion(&s[i]);
 	}
-	_putchar('\n');
+	if (s[i] == '\0')
+		_putchar('\n');
 }
