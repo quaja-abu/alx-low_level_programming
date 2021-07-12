5e23c519031cfe7189a9114c776ae04157ab3bad
Bantegize Addis Alemayehu <acabuchane@gmail.com>
2021-07-12 08:25:42 +0000

    fix betty style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
-  * main - check the code
+  * main - function to check program name
+  * @argc: integer value that holds number of arguments
+  * @argv: character pointer that holds arguments
   *
   * Return: Always 0
   */
 int main(__attribute__((unused)) int argc, char **argv)
 {
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
