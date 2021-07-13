b40a10a3338a022e76ef3ee3167ec77cf032176f
Bantegize Addis Alemayehu <acabuchane@gmail.com>
2021-07-13 19:50:34 +0000

    fix memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdlib.h>
 #include <stddef.h>
-
+int **assign(int **arr, int m, int n);
 /**
   * alloc_grid - function that returns a pointer to a 2 dimensional array
   * @row: integer value
@@ -13,7 +13,6 @@ int **alloc_grid(int row, int col)
 {
 	int **ptr;
 	int i;
-	int j;
 
 	if (row <= 0 || col <= 0)
 	{
@@ -22,16 +21,33 @@ int **alloc_grid(int row, int col)
 	else
 	{
 		ptr = (int **)malloc(sizeof(int *) * row);
+		*ptr = (int *)malloc(sizeof(int) * row * col);
 		for (i = 0; i < row; i++)
-			*(ptr + i) = (int *)malloc(sizeof(int) * col);
-		for (i = 0; i < row; i++)
+			ptr[i] = (*ptr + col * i);
+		ptr = assign(ptr, row, col);
+	}
+	free(*ptr);
+	return (ptr);
+}
+/**
+  * assign - function called by above function
+  * @arr: integer pointer of pointer
+  * @m: integer value
+  * @n: integer value
+  *
+  * Return: integer pointer of pointer
+  */
+int **assign(int **arr, int m, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < m; i++)
+	{
+		for (j = 0; j < n; j++)
 		{
-			for (j = 0; j < col; j++)
-				ptr[i][j] = 0;
+			*(*(arr + i) + j) = 0;
 		}
 	}
-	for (i = 0; i < row; i++)
-		free(ptr[i]);
-	return (ptr);
-	free(ptr);
+	return (arr);
 }
