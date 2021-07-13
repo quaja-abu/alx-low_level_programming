0e6c20248a7e2cdfc3efce7b0c60a9eedab9a7c6
Bantegize Addis Alemayehu <acabuchane@gmail.com>
2021-07-13 17:15:16 +0000

    fix memory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 #include <stdlib.h>
 #include <stddef.h>
+char *concat(char *, char *, char *, int, int, int, int);
 
 /**
   * str_concat - function that concatenates two strings
@@ -12,40 +13,65 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s;
+	char *val;
 	int size1 = 0;
 	int size2 = 0;
 	int sizet;
 	int i = 0;
 	int j = 0;
 
-	if ((s1 == NULL) && (s2 == NULL))
+	if ((s1 == (char *)NULL) && (s2 == (char *)NULL))
 	{
 		return (NULL);
 	}
 	else
 	{
-		while (s1[size1] != '\0')
+		if (s1 != (char *)NULL)
 		{
-			size1++;
+			while (s1[size1] != '\0')
+			{
+				size1++;
+			}
 		}
-		while (s2[size2] != '\0')
+		if (s2 != (char *)NULL)
 		{
-			size2++;
+			while (s2[size2] != '\0')
+			{
+				size2++;
+			}
 		}
 		sizet = (size1 + size2 + sizeof(char));
 		s = malloc(sizeof(char) * sizet);
-		while (i < size1)
-		{
-			s[i] = s1[i];
-			i++;
-		}
-		while (i < sizet)
-		{
-			s[i] = s2[j];
-			i++;
-			j++;
-		}
-		s[i - 1] = '\0';
 	}
+	val = concat(s, s1, s2, sizet, size1, i, j);
+	return (val);
+}
+
+/**
+  * concat - function to be called by the above function
+  * @s: character pointer
+  * @s1: integer value
+  * @s2: integer value
+  * @sizet: interger value
+  * @size1: integer value
+  * @i: integer value
+  * @j: integer value
+  *
+  * Return: character pointer
+  */
+char *concat(char *s, char *s1, char *s2, int sizet, int size1, int i, int j)
+{
+	while (i < size1)
+	{
+		s[i] = s1[i];
+		i++;
+	}
+	while (i < sizet)
+	{
+		s[i] = s2[j];
+		i++;
+		j++;
+	}
+	s[i] = '\0';
 	return (s);
 }
