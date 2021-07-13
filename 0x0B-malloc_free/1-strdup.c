ae2f83ba57dfbeef70d1e0cba230768e9c84d8b2
Bantegize Addis Alemayehu <acabuchane@gmail.com>
2021-07-13 14:45:08 +0000

    program that returns a pointer to a newly allocated space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+  * _strdup - function that returns a pointer to a newly allocated space
+  * @str: character pointer for duplicate value
+  *
+  * Return: character pointer
+  */
+char *_strdup(char *str)
+{
+	char *s;
+	int size = 0;
+	int i = 0;
+
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		s = malloc(sizeof(char) * size);
+		if (s == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			while (i < size)
+			{
+				s[i] = str[i];
+				i++;
+			}
+		}
+	}
+	return (s);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -3,5 +3,6 @@
 
 int _putchar(char c);
 char *create_array(unsigned int, char);
+char *_strdup(char *str);
 
 #endif /* _HOLBERTON_H_*/
