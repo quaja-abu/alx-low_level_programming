dc43afda3298057083abaae71fd80779f38a1990
Bantegize Addis Alemayehu <acabuchane@gmail.com>
2021-07-14 16:45:35 +0000

    fix code

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,85 +1,47 @@
 #include "holberton.h"
 #include <stdlib.h>
-#include <stddef.h>
-char *concat(char *, char *, char *, int, int, int, int);
 
 /**
-  * str_concat - function that concatenates two strings
-  * @s1: character pointer
-  * @s2: character pointer
-  *
-  * Return: character pointer
-  */
+ * str_concat - Concatenates two strings using at
+ *                  most an inputted number of bytes.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - a pointer to the concatenated space in memory.
+ */
 char *str_concat(char *s1, char *s2)
 {
-	char *s;
-	char *val = "";
-	int size1 = 0;
-	int size2 = 0;
-	int sizet;
-	int i = 0;
-	int j = 0;
+	char *concat;
+	int len1 = 0, len2 = 0, lent, index;
 
-	if ((s1 == (char *)NULL) && (s2 == (char *)NULL))
-	{
-		return (val);
-	}
-	else
-	{
-		if (s1 != (char *)NULL)
-		{
-			while (s1[size1] != '\0')
-			{
-				size1++;
-			}
-		}
-		if (s2 != (char *)NULL)
-		{
-			while (s2[size2] != '\0')
-			{
-				size2++;
-			}
-		}
-		sizet = (size1 + size2 + sizeof(char));
-		s = malloc(sizeof(char) * sizet);
-		if (s == NULL)
-		{
-			return (NULL);
-		}
-	}
-	val = concat(s, s1, s2, sizet, size1, i, j);
-	free(val);
-	return (val);
-}
+	if (s1 == NULL)
+		s1 = "";
 
-/**
-  * concat - function to be called by the above function
-  * @s: character pointer
-  * @s1: integer value
-  * @s2: integer value
-  * @sizet: interger value
-  * @size1: integer value
-  * @i: integer value
-  * @j: integer value
-  *
-  * Return: character pointer
-  */
-char *concat(char *s, char *s1, char *s2, int sizet, int size1, int i, int j)
-{
-	while (i < size1)
-	{
-		s[i] = s1[i];
-		i++;
-	}
-	if (s2 != (char *)NULL)
-	{
-		while (i < sizet)
-		{
-			s[i] = s2[j];
-			i++;
-			j++;
-		}
-	}
-	s[i] = '\0';
-	return (s);
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index]; index++)
+		len1++;
+
+	for (index = 0; s2[index]; index++)
+		len2++;
+
+	lent = (sizeof(char) + len1 + len2);
+	concat = malloc(sizeof(char) * lent);
+
+	if (concat == NULL)
+		return (NULL);
+
+	lent = 0;
+
+	for (index = 0; s1[index]; index++)
+		concat[lent++] = s1[index];
+
+	for (index = 0; s2[index]; index++)
+		concat[lent++] = s2[index];
+
+	concat[lent] = '\0';
+
+	return (concat);
 }
