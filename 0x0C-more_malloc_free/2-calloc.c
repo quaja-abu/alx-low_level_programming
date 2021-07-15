261fafd31a34bc1c57a37d8c14efbe7bcaf9b34d
quaja-abu <acabuchane@gmail.com>
2021-07-15 18:33:02 +0300

    program that allocates memory for an array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+/*
+ * File: 2-calloc.c
+ * Author: Bantegize Addis
+ */
+
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * _calloc - function that allocates memory for an array using malloc
+  * @nmemb: integer value
+  * @size: integer value
+  *
+  * Return: pointer
+  */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *c;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	c = malloc(size * nmemb);
+	if (c == NULL)
+		return (NULL);
+	return (c);
+
+}
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,12 +1,15 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 /*
  * File: holberton.h
  * Author: Bantegize Addis
  */
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
 
 int _putchar(char);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif /* _HOLBERTON_H_*/
