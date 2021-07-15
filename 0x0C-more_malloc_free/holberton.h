05031133f603d1f15406b5db25ee0fc5099b4d82
quaja-abu <acabuchane@gmail.com>
2021-07-15 14:41:24 +0300

    adding header file

---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,12 @@
+/*
+ * File: holberton.h
+ * Author: Bantegize Addis
+ */
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+int _putchar(char);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+#endif /* _HOLBERTON_H_*/
