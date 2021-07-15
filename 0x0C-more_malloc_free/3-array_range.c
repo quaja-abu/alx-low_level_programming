00b4b3966e76b006833ab7cc3e39239ca70d0ed6
quaja-abu <acabuchane@gmail.com>
2021-07-15 19:14:44 +0300

    fix code

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -6,6 +6,7 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
   * array_range - function that creates an array of integers
@@ -15,7 +16,7 @@
   * Return: pointer to the newly created array
   */
 
-int *array_range(unsigned int min, unsigned int max)
+int *array_range(int min, int max)
 {
 	int *ptr;
 	int size = 0;
