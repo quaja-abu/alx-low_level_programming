7072292c85524ab1a11da3ac28c57fcd33312ad2
quaja-abu <acabuchane@gmail.com>
2021-07-19 15:38:05 +0300

    header file that define a new type struct dog with three elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+/**
+  * struct dog - structure to define three values
+  * @name: character pointer
+  * @age: float value
+  * @owner: character pointer
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _HEADER_H_*/
