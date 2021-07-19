ae6d65b6053b0c9480d11310ca195b9e84ad4cc5
quaja-abu <acabuchane@gmail.com>
2021-07-19 22:39:48 +0300

    fix code

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,7 @@
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d = malloc(sizeof(dog_t) * 3);
+	dog_t *d = (dog_t *)malloc(sizeof(dog_t));
 
 	if (d == NULL)
 		return (NULL);
