cad4345c41d14cd0b1009643b1d98bfe35e08d27
quaja-abu <acabuchane@gmail.com>
2021-07-20 15:10:05 +0300

    fix code

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -7,10 +7,9 @@
   */
 void free_dog(dog_t *d)
 {
-	if (d != NULL)
-		free(d);
-	if (d->name != NULL)
-		free(d->name);
-	if (d->owner != NULL)
-		free(d->owner);
+	if (d == NULL)
+		return;
+	free(d);
+	free(d->name);
+	free(d->owner);
 }
