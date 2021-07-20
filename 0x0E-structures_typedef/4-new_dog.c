8b613fbef6d1bd19251ef520e2e7869dd4ab3c84
quaja-abu <acabuchane@gmail.com>
2021-07-20 14:56:29 +0300

    fix code

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,8 @@
 #include "dog.h"
 #include <stddef.h>
 #include <stdlib.h>
+int _strlen(char *str);
+char *_strcpy(char *dest, char *src);
 
 /**
   * new_dog - function that creates a new dog
@@ -13,41 +15,68 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
-	int nlen, olen, i = 0;
-
-	while (name[i] != '\0')
-	{
-		nlen++;
-		i++;
-	}
-	i = 0;
-	while (owner[i] != '\0')
-	{
-		olen++;
-		i++;
-	}
+	char *cpname = "", *cpowner = "";
+	int len;
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
+		return (NULL);
+	len = _strlen(name);
+	d->name = malloc(sizeof(char) * len + 1);
+	if (d->name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
-	d->name = malloc(sizeof(char) * nlen);
-	if (d->name == NULL)
+	len = _strlen(owner);
+	d->owner = malloc(sizeof(char) * len + 1);
+	if (d->owner == NULL)
 	{
+		free(d);
 		free(d->name);
 		return (NULL);
 	}
-	d->owner = malloc(sizeof(char) * olen);
-	if (d->owner == NULL)
-	{
-		free(d->owner);
-		return (NULL);
-	}
 
-	d->name = name;
+	cpname = _strcpy(d->name, name);
+	cpowner = _strcpy(d->owner, owner);
+	d->name = cpname;
 	d->age = age;
-	d->owner = owner;
+	d->owner = cpowner;
+
 	return (d);
+}
+/**
+  * _strlen - function to find the length of a string
+  * @str: character pointer
+  *
+  * Return: integer value
+  */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+  * _strcpy - function to copy src string to dest
+  * @dest: charcater pointer
+  * @src: character pointer
+  *
+  * Return: character pointer
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
