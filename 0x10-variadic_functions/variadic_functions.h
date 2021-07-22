75d22202b0451b4b431836848e39c9306de4f37b
quaja-abu <acabuchane@gmail.com>
2021-07-22 15:14:21 +0300

    update header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,5 +5,6 @@
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *, const unsigned int, ...);
+void print_strings(const char *, const unsigned int, ...);
 
 #endif /* _VARIADIC_H_*/
