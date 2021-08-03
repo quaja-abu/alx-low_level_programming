03cdc56feb21c653c0a450d614f2de481b2ba98d
quaja-abu <acabuchane@gmail.com>
2021-08-03 16:46:18 +0300

    updating the header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -19,5 +19,6 @@ typedef struct listint_s
 int _putchar(char);
 size_t print_listint(const listint_t *);
 size_t listint_len(const listint_t *);
+listint_t *add_nodeint(listint_t **, const int);
 #endif
 
