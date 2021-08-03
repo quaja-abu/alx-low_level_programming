079719bf63715c8e01d8bd3250ac38821f738e92
quaja-abu <acabuchane@gmail.com>
2021-08-03 16:03:04 +0300

    program that displays a singly linked list of type listint_t

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  * print_listint - function that prints all the elements of a lisint_t list
+  * @h: costant listint_t structure
+  *
+  * Return: the number of nodes
+  */
+size_t print_listint(const listint_t *h)
+{
+	size_t n = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
