8d07879f7dbed07678cd3c4605e51fcc187fc96c
quaja-abu <acabuchane@gmail.com>
2021-08-06 19:45:55 +0300

    fix code

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,9 +13,11 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int val = 0;
 	int i = 0;
 
+	if (b == NULL)
+		return (0);
 	while (b[i] != '\0')
 	{
-		if (b == NULL || (b[i] != '0' && b[i] != '1'))
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		i++;
 	}
