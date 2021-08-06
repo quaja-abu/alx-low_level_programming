1f5bb403d2cf2607d2a754018542d5b375eb177f
quaja-abu <acabuchane@gmail.com>
2021-08-06 21:17:09 +0300

    update the header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,4 +5,8 @@
 int _putchar(char);
 unsigned int binary_to_uint(const char *);
 void print_binary(unsigned long int);
+int get_bit(unsigned long int, unsigned int);
+int set_bit(unsigned long int *, unsigned int);
+int clear_bit(unsigned long int *, unsigned int);
+unsigned int flip_bits(unsigned int, unsigned long int);
 #endif
